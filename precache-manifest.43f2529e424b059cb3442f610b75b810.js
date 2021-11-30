023a55bd5a2cb798fdf64295167f350f63cccc68
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2021-11-30 20:38:21 +0100

    Updates

---
+++ precache-manifest.43f2529e424b059cb3442f610b75b810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9d491ce4dfff580a697739614b4f08",
+    "url": "/Grocery-Bud/index.html"
+  },
+  {
+    "revision": "69641551a9ecf3a18801",
+    "url": "/Grocery-Bud/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/Grocery-Bud/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Grocery-Bud/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69641551a9ecf3a18801",
+    "url": "/Grocery-Bud/static/js/main.1b8e7b95.chunk.js"
+  },
+  {
+    "revision": "a9e8964a3c386650e734",
+    "url": "/Grocery-Bud/static/js/runtime-main.1f803fac.js"
+  }
+]);
